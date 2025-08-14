32e61fc4fbd3d4aefc6581ca9b4ebaa1bea7faa5
Funke Sowole <funkegoodvibe@gmail.com>
2025-08-14 20:36:31 +0100

    add url and randomiser stub

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 import requests
 import random
 
-word_list = {
+dict_word_list = {
 
     "amaze": 1240,
     "amazement":3333
@@ -23,11 +23,25 @@ while True:
 
     sentence = input("Say something: ")
 
-    my_dict = list(word_list.keys())
+    my_dict = list(dict_word_list.keys())
 
     for word in my_dict:
         if word in sentence: 
-            print("found",word)
+
+            #temp: print the word if found
+            print("found", word)
+            
+            #get the index of the 
+            index = dict_word_list[word]
+
+            #generate a random number 1/2 for old and new testament
+            random_int = random.randint(1,2)
+
+            #construct the concordence urls 
+            url_base_ot = url_base_ot.format(word[0], index)
+            url_base_nt = url_base_nt.format(word[0], index)
+            url_base_all = url_base_all.format(word[0], index, random_int)
+
 
 
     
