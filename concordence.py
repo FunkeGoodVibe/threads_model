ba43f9e3c0cb515d7027bd868ea6dd10df552f6d
Funke Sowole <funkegoodvibe@gmail.com>
2025-08-24 00:11:08 +0100

    add bs4 line

---
+++ concordence.py
@@ -0,0 +1,2 @@
+# https://thekingsbible.com/Concordance/faith - GOOD uses words (weakness : styling ?)
+# https://www.artbible.info/concordance/a.html - GOOD less styling (weakness : )
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ####
 #
 ####
-###
+#check out fast-api
 from bs4 import BeautifulSoup
 import requests
 import random
@@ -10,27 +10,39 @@ dict_word_list = {
 
     "amaze": 1240,
     "amazement":3333
-
+    
 }
 
 #0=first character; 1=index; 2=nt/ot
 #"https://www.artbible.info/concordance/a/1240-2.html"
-url_base_ot = r"https://www.artbible.info/concordance/{0}/{1}-1.html"
-url_base_nt = r"https://www.artbible.info/concordance/{0}/{1}-2.html"
-url_base_all = r"https://www.artbible.info/concordance/{0}/{1}-{2}.html"
 
-while True: 
+def speak(): 
+
+    """
+    0) stores the concordence url for new/old testament (and randomiser)
+    1) takes input from the user
+    2) construct the concordence url, for either the new/old testament using randomiser
+    """
+
+    url_base_ot = r"https://www.artbible.info/concordance/{0}/{1}-1.html"
+    url_base_nt = r"https://www.artbible.info/concordance/{0}/{1}-2.html"
+    url_base_all = r"https://www.artbible.info/concordance/{0}/{1}-{2}.html"
 
     sentence = input("Say something: ")
 
     my_dict = list(dict_word_list.keys())
 
     for word in my_dict:
+
+        # word=r"\s{0}\s".format(word) #word separated by spaces
+        # x = re.search(word, sentence)
+        #if x.group() in sentence: 
+        
         if word in sentence: 
 
             #temp: print the word if found
             print("found", word)
-            
+                
             #get the index of the 
             index = dict_word_list[word]
 
@@ -42,6 +54,41 @@ while True:
             url_base_nt = url_base_nt.format(word[0], index)
             url_base_all = url_base_all.format(word[0], index, random_int)
 
+            #determine which url to use 
+            url = url_base_all 
+
+        return url
 
 
+def get_verse(url):
+
+    """
+    1) 
+    2) 
+    3) 
+    """
+
+    r = requests.get(url)
+    html_doc = r.text
+
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    #raw_strip = soup.find("div", {"id": "contentkol"})
+    raw_strip = soup.find(attrs={'id':'contentkol'})
+    #print(raw_strip)
+
+    p_el = soup.find_all('p')
+    el_list = []
+    for element in p_el:
+        el_list.append(element)
+
+    print(el_list[1])
+
+
+
+if __name__ == "__main__":
+
+    while True: 
+        url = speak()
+        get_verse(url)
     
+
